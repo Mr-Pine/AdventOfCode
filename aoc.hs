4604ba3b27d4557e0cd3b8fd352cb607e8e565fd
Mr-Pine <git@mr-pine.de>
2023-12-01 20:10:20 +0100

    Nice program

--- aoc.hs
+++ aoc.hs
@@ -1,39 +1,30 @@
-import Data.Char (isDigit, readLitChar)
-import Data.List
-import Distribution.Simple.Utils (debug)
-import Debug.Trace (trace)
-main :: IO ()
+import Data.Char (isDigit)
+import Data.List (findIndex, isPrefixOf)
+
+
 main = do
-    readFile "./input/1.input" >>= print . d1p1
-    readFile "./input/1.input" >>= print . d1p2
-    readFile "./input/1.example" >>= print . d1p2
+    input <- readFile "./input/1.input"
+    let partOneData = map extractNumbers (lines input)
+    let partTwoData = map (extractNumbers . transform) (lines input)
+    print $ day1 partOneData
+    print $ day1 partTwoData
 
+day1 :: [String] -> Int
+day1 = sum . map (read . (\numbers -> [head numbers, last numbers]))
 
-d1p1 :: String -> Int
-d1p1 = sum . map (convertLineToNumber extractNumbersSimple) . lines
+numberStrings :: [String]
+numberStrings = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
-d1p2 :: String -> Int
-d1p2 = sum . map (convertLineToNumber hardProxy) . lines
+transform :: String -> String
+transform (x:xs) | isDigit x = x : transform xs
+transform l@(x:xs) = findNumber l ++ transform xs
+transform [] = []
 
-convertLineToNumber :: (String -> String) -> String -> Int
-convertLineToNumber transformer = read . (\numbers -> [head numbers, last numbers]) . transformer
+findNumber :: String -> String
+findNumber xs = maybe "" (show . (+1)) (getIndex xs)
 
-extractNumbersSimple :: String -> String
-extractNumbersSimple = filter isDigit
+getIndex :: String -> Maybe Int
+getIndex xs = findIndex (`isPrefixOf` xs) numberStrings
 
-hardProxy :: String -> String
-hardProxy string = extractNumbersHard string--trace ("Test: " ++ string) extractNumbersHard string
-
-extractNumbersHard :: String -> String
-extractNumbersHard ('o':'n':'e':xs) = '1' : hardProxy ("ne" ++ xs)
-extractNumbersHard ('t':'w':'o':xs) = '1' : hardProxy ("wo" ++ xs)
-extractNumbersHard ('t':'h':'r':'e':'e':xs) = '1' : hardProxy ("hree" ++ xs)
-extractNumbersHard ('f':'o':'u':'r':xs) = '1' : hardProxy ("our" ++ xs)
-extractNumbersHard ('f':'i':'v':'e':xs) = '1' : hardProxy ("ive" ++ xs)
-extractNumbersHard ('s':'i':'x':xs) = '1' : hardProxy ("ix" ++ xs)
-extractNumbersHard ('s':'e':'v':'e':'n':xs) = '1' : hardProxy ("even" ++ xs)
-extractNumbersHard ('e':'i':'g':'h':'t':xs) = '1' : hardProxy ("ight" ++ xs)
-extractNumbersHard ('n':'i':'n':'e':xs) = '1' : hardProxy ("ine" ++ xs)
-extractNumbersHard (x:xs) | isDigit x = x : hardProxy xs
-extractNumbersHard (x:xs) = hardProxy xs
-extractNumbersHard [] = []
+extractNumbers :: String -> String
+extractNumbers = filter isDigit
