5cc6bca09aaa85c893129b01f23d686ff3bc356f
Mr-Pine <git@mr-pine.de>
2023-12-04 12:01:27 +0100

    Cleanup the second

--- app/Day4/Scratchcards.hs
+++ app/Day4/Scratchcards.hs
@@ -1,32 +1,38 @@
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module Day4.Scratchcards (solveDay4) where
 
-import Util (example, Parser, parseOrError, debugMessage, input, debugMessageWith)
-import Text.Megaparsec.Char (string, space, char, newline, hspace)
+import Data.List (intersect)
+import Text.Megaparsec (endBy, sepBy, sepBy1)
+import Text.Megaparsec.Char (char, hspace, newline, space, string)
 import Text.Megaparsec.Char.Lexer (decimal)
-import Text.Megaparsec (sepBy, sepBy1, endBy)
+import Util (Parser, debugMessage, debugMessageWith, example, input, parseOrError)
 
 solveDay4 = do
   putStrLn "Day 4 - Gear Ratios:"
-  input <- input 4
+  input <- example 4
   scratchcards <- parseOrError scratchcardsParser input
   print $ part1 scratchcards
   print $ part2 scratchcards
 
 part1 = sum . map (debugMessage "Value " . value)
+
 part2 cards = replaceCards cards cards
 
-data Scratchcard = Scratchcard {
-    index :: Int,
+data Scratchcard = Scratchcard
+  { index :: Int,
     cardNumbers :: [Int],
     winningNumbers :: [Int]
-} deriving Show
+  }
+  deriving (Show)
 
 value :: Scratchcard -> Int
 value card = 2 `power` (length (wonNumbers card) - 1)
 
 power x y
-    | y < 0 = 0
-    | otherwise = x ^ y
+  | y < 0 = 0
+  | otherwise = x ^ y
 
 scratchcardsParser :: Parser [Scratchcard]
 scratchcardsParser = scratchcardParser `sepBy1` newline
@@ -37,11 +43,11 @@ scratchcardParser = Scratchcard <$> index <*> (decimal `endBy` space <* space <*
     index = string "Card" *> space *> decimal <* char ':' <* space
 
 replaceCards :: [Scratchcard] -> [Scratchcard] -> Int
-replaceCards full (x@Scratchcard{index = base}:xs) = 1 + replaceCards full (debugMessageWith ("next Cards from Card " ++ (show . index) x ++ ": ") (show . map (show .index)) additional) + replaceCards full xs
+replaceCards full (x : xs) = 1 + replaceCards full (debugMessageWith ("next Cards from Card " ++ (show . index) x ++ ": ") (show . map (show . index)) additional) + replaceCards full xs
   where
-    additional = (map ((full!!) . (+ base)) . getNextIndices) x
+    additional = (map ((full !!) . (+ x.index)) . getNextIndices) x
 replaceCards _ [] = 0
 
-getNextIndices = enumFromTo 0 . subtract 1 . length . wonNumbers
+getNextIndices x = [0 .. length (wonNumbers x) - 1]
 
-wonNumbers Scratchcard{cardNumbers = cardNumbers, winningNumbers = winningNumbers} = filter (`elem` winningNumbers) cardNumbers
+wonNumbers Scratchcard {..} = winningNumbers `intersect` cardNumbers
