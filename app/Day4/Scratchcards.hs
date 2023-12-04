54eab7e4cbe245f8c687e6f95697c5a84bedc551
Mr-Pine <git@mr-pine.de>
2023-12-04 12:21:12 +0100

    I am speed

--- app/Day4/Scratchcards.hs
+++ app/Day4/Scratchcards.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE RecordWildCards #-}
 
 module Day4.Scratchcards (solveDay4) where
@@ -11,14 +10,14 @@ import Util (Parser, debugMessage, debugMessageWith, example, input, parseOrErro
 
 solveDay4 = do
   putStrLn "Day 4 - Gear Ratios:"
-  input <- example 4
+  input <- input 4
   scratchcards <- parseOrError scratchcardsParser input
   print $ part1 scratchcards
   print $ part2 scratchcards
 
-part1 = sum . map (debugMessage "Value " . value)
+part1 = sum . map value
 
-part2 cards = replaceCards cards cards
+part2 cards = countCards cards (replicate (length cards) 1)
 
 data Scratchcard = Scratchcard
   { index :: Int,
@@ -28,7 +27,7 @@ data Scratchcard = Scratchcard
   deriving (Show)
 
 value :: Scratchcard -> Int
-value card = 2 `power` (length (wonNumbers card) - 1)
+value card = 2 `power` (wonAmount card - 1)
 
 power x y
   | y < 0 = 0
@@ -42,12 +41,11 @@ scratchcardParser = Scratchcard <$> index <*> (decimal `endBy` space <* space <*
   where
     index = string "Card" *> space *> decimal <* char ':' <* space
 
-replaceCards :: [Scratchcard] -> [Scratchcard] -> Int
-replaceCards full (x : xs) = 1 + replaceCards full (debugMessageWith ("next Cards from Card " ++ (show . index) x ++ ": ") (show . map (show . index)) additional) + replaceCards full xs
+countCards :: [Scratchcard] -> [Int] -> Int
+countCards (x:xs) (c:cs) = c + countCards xs counts
   where
-    additional = (map ((full !!) . (+ x.index)) . getNextIndices) x
-replaceCards _ [] = 0
+    counts = nextCounts ++ drop (wonAmount x) cs
+    nextCounts = map (+c) (take (wonAmount x) cs)
+countCards [] [] = 0
 
-getNextIndices x = [0 .. length (wonNumbers x) - 1]
-
-wonNumbers Scratchcard {..} = winningNumbers `intersect` cardNumbers
+wonAmount Scratchcard {..} = length (winningNumbers `intersect` cardNumbers)
