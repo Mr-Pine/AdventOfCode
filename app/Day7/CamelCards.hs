37641267dbdea3db5ac31f2e534e03c0cd16e61e
Mr-Pine <git@mr-pine.de>
2023-12-08 02:39:04 +0100

    Cleanup

--- app/Day7/CamelCards.hs
+++ app/Day7/CamelCards.hs
@@ -1,99 +1,97 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Day7.CamelCards (solveDay7) where
 
 import Util (example, Parser, parseOrError, Prettify (prettify), input)
 import Text.Megaparsec.Char (space, char, digitChar)
 import qualified Text.Megaparsec.Char.Lexer as L
 import Text.Megaparsec (sepEndBy, count, (<|>), ErrorItem (Label))
-import Data.List (singleton, group, partition, sort, sortBy, find, maximumBy)
+import Data.List (sortOn, sort, group, maximumBy)
 import Data.Maybe (fromMaybe, listToMaybe)
 import Data.Function (on)
+import Data.Coerce (coerce)
+import qualified Data.Ord
+import Data.Ord (comparing)
+import Data.Char (digitToInt)
 
 solveDay7 = do
   input <- input 7
   putStrLn "Day 7 - Camel Cards:"
-  entries1 <- parseOrError (entriesParser classifyPart1) input
-  entries2 <- parseOrError (entriesParser classifyPart2) input
-  print $ solve part1LabelOrder entries1
-  print $ solve part2LabelOrder entries2
+  entries1 <- parseOrError (entriesParser LabelPart1) input
+  entries2 <- parseOrError (entriesParser LabelPart2) input
+  print $ solve entries1
+  print $ solve entries2
 
-solve labelOrder = sum . zipWith (*) [1..] . map bid . sortBy entryOrder
-  where
-    entryOrder :: Entry -> Entry -> Ordering
-    entryOrder = handComparison `on` hand
+solve :: Label a => [Entry a] -> Int
+solve = sum . zipWith (*) [1..] . map bid . sortOn hand
 
-    handComparison = compareHands (compareBy labelOrder)
-
-    compareHands comparing a@Hand{handType = handType1, labels = label1} b@Hand{handType = handType2, labels = label2}
-      | handType1 == handType2 = comparing label1 label2
-      | otherwise = compare handType1 handType2
-
-    compareBy ordering xs ys = fromMaybe EQ . find (/=EQ) $ zipWith ordering xs ys
-
-type Classifier = [Label] -> Type
-classifyPart1 :: Classifier
-classifyPart1 = classifyGroups . group . sort
-  where
-    classifyGroups xs
-      | length xs == 1 = FiveOfAKind
-      | any ((==4) . length) xs = FourOfAKind
-      | any ((==3) . length) xs && any ((==2) . length) xs = FullHouse
-      | any ((==3) . length) xs = ThreeOfAKind
-      | (length . filter ((==2) . length)) xs == 2 = TwoPair
-      | any ((==2) . length) xs = OnePair
-      | otherwise = HighCard
-
-
-classifyPart2 :: Classifier
-classifyPart2 labels = let
-    mostCommon = maybe J (fromMaybe J . listToMaybe . maximumBy (compare `on` length)) . maybeNotEmpty . group . sort . filter (/=J) $ labels
-  in classifyPart1 . map (\l -> if l == J then mostCommon else l) $ labels
-  where
-    maybeNotEmpty [] = Nothing
-    maybeNotEmpty xs = Just xs
-
-
-type LabelOrder = Label -> Label -> Ordering
-part1LabelOrder :: LabelOrder
-part1LabelOrder = compare
-part2LabelOrder :: LabelOrder
-part2LabelOrder J J = EQ
-part2LabelOrder J _ = LT
-part2LabelOrder _ J = GT
-part2LabelOrder x y = compare x y
-
-data Entry = Entry
-  { hand :: Hand,
+data Entry a = Entry
+  { hand :: Hand a,
     bid :: Int
   } deriving (Show, Eq)
 
-data Hand = Hand
-  { labels :: [Label],
-    handType :: Type
-  } deriving (Show, Eq)
+newtype Label a => Hand a = Hand {labels :: [a]} deriving (Show, Eq)
 
+instance (Label a) => Ord (Hand a) where
+  compare a b = compare (classify (labels a), labels a) (classify (labels b), labels b)
 
-data Label = Value Int | T | J | Q | K | A deriving (Show, Eq, Ord)
+class (Show a, Eq a, Ord a) => Label a where
+  classify :: [a] -> Type
 
-instance Prettify Label where
+data LabelBase = Value Int | T | J | Q | K | A deriving (Show, Eq, Ord)
+newtype LabelPart1 = LabelPart1 LabelBase deriving (Show, Eq, Prettify, Ord)
+newtype LabelPart2 = LabelPart2 LabelBase deriving (Show, Eq, Prettify)
+
+instance Label LabelPart1 where
+  classify = classifyLengths . sortOn Data.Ord.Down . map length . group . sort
+    where
+      classifyLengths [5] = FiveOfAKind
+      classifyLengths [4,_] = FourOfAKind
+      classifyLengths [3,2] = FullHouse
+      classifyLengths [3,_,_] = ThreeOfAKind
+      classifyLengths [2,2,_] = TwoPair
+      classifyLengths (2:_) = OnePair
+      classifyLengths _ = HighCard
+
+instance Label LabelPart2 where
+  classify labels = classify . map (replace . toPart1) $ labels
+    where
+      mostCommon = maybe (LabelPart2 J) (fromMaybe (LabelPart2 J) . listToMaybe . maximumBy (compare `on` length)) . maybeNotEmpty . group . sort . filter (/=LabelPart2 J) $ labels
+
+      replace :: LabelPart1 -> LabelPart1
+      replace (LabelPart1 J) = toPart1 mostCommon
+      replace x = x
+
+      toPart1 :: LabelPart2 -> LabelPart1
+      toPart1 l = LabelPart1 (coerce l :: LabelBase)
+
+      maybeNotEmpty [] = Nothing
+      maybeNotEmpty xs = Just xs
+
+instance Ord LabelPart2 where
+  compare (LabelPart2 J) (LabelPart2 J) = EQ
+  compare (LabelPart2 J) _ = LT
+  compare _ (LabelPart2 J) = GT
+  compare x y = compare (coerce x :: LabelBase) (coerce y :: LabelBase)
+
+instance Prettify LabelBase where
     prettify = show
 
 data Type = HighCard | OnePair | TwoPair | ThreeOfAKind | FullHouse | FourOfAKind | FiveOfAKind deriving (Show, Eq,Ord)
 
-entriesParser :: Classifier -> Parser [Entry]
-entriesParser classify = entryParser classify `sepEndBy` space
+type LabelConstructor a = (LabelBase -> a)
+entriesParser :: Label a => LabelConstructor a -> Parser [Entry a]
+entriesParser constructor = entryParser constructor `sepEndBy` space
 
-entryParser :: Classifier -> Parser Entry
-entryParser classify = Entry <$> handParser classify <* space <*> L.decimal
+entryParser :: Label a => LabelConstructor a -> Parser (Entry a)
+entryParser constructor = Entry <$> handParser constructor <* space <*> L.decimal
 
-handParser :: Classifier -> Parser Hand
-handParser classify = getHand <$> count 5 labelParser
+handParser :: Label a => LabelConstructor a -> Parser (Hand a)
+handParser construct = Hand <$> count 5 (construct <$> labelParser)
     where
-        labelParser = parseA <|> parseK <|> parseQ <|> parseJ <|> parseT <|> parseT <|> (parseValueLabel <$> digitChar) :: Parser Label
-        parseA = A <$ char 'A' :: Parser Label
-        parseK = K <$ char 'K' :: Parser Label
-        parseQ = Q <$ char 'Q' :: Parser Label
-        parseJ = J <$ char 'J' :: Parser Label
-        parseT = T <$ char 'T' :: Parser Label
-        parseValueLabel = Value . read . singleton
-
-        getHand labels = Hand labels (classify labels)
+        labelParser = parseA <|> parseK <|> parseQ <|> parseJ <|> parseT <|> parseT <|> (parseValueLabel <$> digitChar) :: Parser LabelBase
+        parseA = A <$ char 'A' :: Parser LabelBase
+        parseK = K <$ char 'K' :: Parser LabelBase
+        parseQ = Q <$ char 'Q' :: Parser LabelBase
+        parseJ = J <$ char 'J' :: Parser LabelBase
+        parseT = T <$ char 'T' :: Parser LabelBase
+        parseValueLabel = Value . digitToInt
