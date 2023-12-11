ab0102edb2728d0db670553ee072fae7c8d05c05
Mr-Pine <git@mr-pine.de>
2023-12-11 11:57:42 +0100

    Day 11 Part 2

--- app/Day11/CosmicExpansion.hs
+++ app/Day11/CosmicExpansion.hs
@@ -9,10 +9,12 @@ solveDay11 = do
   putStrLn "Day 11 - Cosmic expansion"
   input <- input 11
   let image = map (map parsePixel) . lines $ input
-  -- putStrLn . intercalate "\n" . map (intercalate "" . map prettify) . expand $ image
   print . part1 $ image
+  print . part2 $ image
 
-part1 = sum . map manhattanDistance . pairings . extractGalaxies . expand
+part1 = solve 2
+part2 = solve 1000000
+solve expansionRatio = sum . map manhattanDistance . pairings . extractGalaxies . enumeratePixels expansionRatio
 
 data Pixel = Emptyness | Galaxy deriving (Show, Eq)
 
@@ -23,23 +25,29 @@ instance Prettify Pixel where
   prettify Galaxy = "#"
   prettify Emptyness = "."
 
-expand :: [[Pixel]] -> [[Pixel]]
-expand = transpose . expandRows . transpose . expandRows
-
-expandRows :: [[Pixel]] -> [[Pixel]]
-expandRows [] = []
-expandRows (x : xs)
-  | all (== Emptyness) x = x : x : expandRows xs
-  | otherwise = x : expandRows xs
-
-extractGalaxies :: [[Pixel]] -> [(Int, Int)]
-extractGalaxies = map snd . filter ((== Galaxy) . fst) . concat . zipWith enumerateHorizontal [0 ..]
-  where
-    enumerateHorizontal i = zipWith (test i) [0 ..]
-    test y x value = (value, (x, y))
+extractGalaxies :: [[(Pixel, (Int, Int))]] -> [(Int, Int)]
+extractGalaxies = map snd . filter ((== Galaxy) . fst) . concat
 
 pairings :: [a] -> [(a, a)]
 pairings [] = []
 pairings (x : xs) = map (x,) xs ++ pairings xs
+
+enumeratePixels :: Int -> [[Pixel]] -> [[(Pixel, (Int, Int))]]
+enumeratePixels expansionRatio pixels = enumerateRows 0 rowEmptyness pixels
+    where
+        getRowEmptyness :: [[Pixel]] -> [Bool]
+        getRowEmptyness = map (all (==Emptyness))
+        rowEmptyness = getRowEmptyness pixels
+        columnEmptyness = getRowEmptyness . transpose $ pixels
+
+        enumerateRows :: Int -> [Bool] -> [[Pixel]] -> [[(Pixel, (Int, Int))]]
+        enumerateRows y (True:es) (r:rs) = enumerateColumns y 0 columnEmptyness r : enumerateRows (y + expansionRatio) es rs
+        enumerateRows y (False:es) (r:rs) = enumerateColumns y 0 columnEmptyness r : enumerateRows (y + 1) es rs
+        enumerateRows _ [] [] = []
+
+        enumerateColumns :: Int -> Int -> [Bool] -> [Pixel] -> [(Pixel, (Int, Int))]
+        enumerateColumns y x (True:es) (c:cs) = enumerateColumns y (x + expansionRatio) es cs
+        enumerateColumns y x (False:es) (c:cs) = (c, (x,y)) : enumerateColumns y (x+1) es cs
+        enumerateColumns _ _ [] [] = []
 
 manhattanDistance ((x1, y1), (x2, y2)) = abs (x1 - x2) + abs (y1 - y2)
