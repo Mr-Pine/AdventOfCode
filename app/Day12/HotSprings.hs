7dc6712e4ab256651cb86b493db786d95cb2d962
MisterPine <git@mr-pine.de>
2023-12-12 14:47:08 +0100

    shitty

--- app/Day12/HotSprings.hs
+++ app/Day12/HotSprings.hs
@@ -4,16 +4,27 @@ import Text.Megaparsec.Char (char, newline, space)
 import Text.Megaparsec ((<|>), many, sepBy, sepEndBy, endBy)
 import qualified Text.Megaparsec.Char.Lexer as L
 import Data.List (intercalate)
+import qualified Data.HashMap.Lazy as Map
+import Data.Maybe (fromMaybe)
+import Data.Hashable (Hashable (hashWithSalt))
+import GHC.Plugins (uncurry3)
 
 solveDay12 = do
     putStrLn "Day 12 - Hot Springs:"
-    input <- input 12
+    input <- example 12
     records <- parseOrError recordsParser input
     print . part1 $ records
     print . part2 $ records
 
 data Spring = Operational | Damaged | Unknown deriving (Show, Eq)
 data Record = Record [Spring] [Int] deriving (Show, Eq)
+
+instance Hashable Spring where
+    hashWithSalt salt spring = hashWithSalt salt (ordinal spring)
+        where
+            ordinal Operational = 0 :: Int
+            ordinal Damaged = 1
+            ordinal Unknown = 2
 
 part1 = sum . map countOptions
 part2 = sum . map (countOptions . unfold)
@@ -25,23 +36,30 @@ recordsParser = recordParser `endBy` newline
         spring = Operational <$ char '.' <|> Damaged <$ char '#' <|> Unknown <$ char '?'
         springParser = many spring
         groupParser = L.decimal `sepBy` char ','
-    
+
+type Key = (Bool, [Spring], [Int])
 countOptions :: Record -> Int
 countOptions (Record springs groups) = countRemainingOptions False springs (groups ++ [0])
     where
+        remainingCounts = Map.empty :: Map.HashMap Key Int
+        getOrCompute key = fromMaybe (debugMessagePlain ("a " ++ show stored) . generator $ key) stored
+            where
+                generator = uncurry3 countRemainingOptions
+                stored = Map.lookup key remainingCounts
+
         --countRemainingOptions :: inGroup: Bool -> springs:[Spring] -> groups:[Int]
         countRemainingOptions :: Bool -> [Spring] -> [Int] -> Int
-        countRemainingOptions False (Operational:xs) gs = countRemainingOptions False xs gs
-        countRemainingOptions True (Operational:xs) (0:gs) = countRemainingOptions False xs gs
+        countRemainingOptions False (Operational:xs) gs = getOrCompute (False, xs, gs)
+        countRemainingOptions True (Operational:xs) (0:gs) = getOrCompute (False, xs, gs)
         countRemainingOptions True (Operational:xs) (_:gs) = 0
 
-        countRemainingOptions False (Damaged:xs) (g:gs) = countRemainingOptions True xs (g-1 : gs)
+        countRemainingOptions False (Damaged:xs) (g:gs) = getOrCompute (True, xs, g-1 : gs)
         countRemainingOptions True (Damaged:xs) (0:gs) = 0
-        countRemainingOptions True (Damaged:xs) (g:gs) = countRemainingOptions True xs (g-1:gs)
+        countRemainingOptions True (Damaged:xs) (g:gs) = getOrCompute (True, xs, g-1:gs)
 
-        countRemainingOptions ig (Unknown:xs) gs = countRemainingOptions ig (Operational:xs) gs + countRemainingOptions ig (Damaged:xs) gs
+        countRemainingOptions ig (Unknown:xs) gs = getOrCompute (ig, Operational:xs, gs) + getOrCompute (ig, Damaged:xs, gs)
 
-        countRemainingOptions ig [] z 
+        countRemainingOptions ig [] z
             | all (==0) z = 1
             | otherwise = 0
 
