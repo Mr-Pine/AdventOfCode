5b4f0f2469ed29c3ff8abf5612b9a87064859678
Mr-Pine <git@mr-pine.de>
2023-12-15 08:44:40 +0100

    Day 14 Part 2

--- app/Day14/ParabolicReflectorDish.hs
+++ app/Day14/ParabolicReflectorDish.hs
@@ -1,50 +1,68 @@
 module Day14.ParabolicReflectorDish (solveDay14) where
-import Util (Parser, example, parseOrError, input, Prettify (prettify))
-import Text.Megaparsec (some, (<|>), sepEndBy)
-import Text.Megaparsec.Char (space, char)
-import Data.List (transpose, intercalate)
-import Data.Sequence (Seq(Empty))
+
+import Data.List (intercalate, transpose)
+import Data.Sequence (Seq (Empty))
+import Text.Megaparsec (sepEndBy, some, (<|>))
+import Text.Megaparsec.Char (char, space)
+import Util (Parser, Prettify (prettify), example, input, parseOrError, debugMessagePlain)
+import qualified Data.Map.Strict as Map
 
 solveDay14 = do
-    putStrLn "Day 14 - Parabolic Reflector Dish:"
-    input <- input 14
-    dish <- parseOrError dishParser input
-    --putStrLn . visualize $ dish
-    --putStrLn "\n"
-    --putStrLn . visualize . tiltNorth $ dish
-    print . day1 $ dish
-    -- print . concatMap prettify . tiltColumnNorth $ [Round, Round, None, Round, None, Round]
+  putStrLn "Day 14 - Parabolic Reflector Dish:"
+  input <- input 14
+  dish <- parseOrError dishParser input
+  print . part1 $ dish
+  print . part2 $ dish
 
-data Rock = Round | Sharp | None deriving (Show, Eq)
+data Rock = Round | Sharp | None deriving (Show, Eq, Ord)
+
 type Dish = [[Rock]]
 
-day1 = load . tiltNorth
+part1 = load . tiltNorth
+part2 = load . tiltCycles 1000000000
 
 visualize :: Dish -> String
 visualize = intercalate "\n" . map (concatMap prettify)
 
 instance Prettify Rock where
-    prettify Round = "O"
-    prettify Sharp = "#"
-    prettify None = "."
+  prettify Round = "O"
+  prettify Sharp = "#"
+  prettify None = "."
 
 tiltNorth :: Dish -> Dish
 tiltNorth = transpose . map tiltColumnNorth . transpose
 
 tiltColumnNorth :: [Rock] -> [Rock]
 tiltColumnNorth = fix rollRocksNorth
-    where
-        rollRocksNorth (None:Round:xs) = Round : rollRocksNorth (None:xs)
-        rollRocksNorth (x:xs) = x : rollRocksNorth xs
-        rollRocksNorth [] = []
+  where
+    rollRocksNorth (None : Round : xs) = Round : rollRocksNorth (None : xs)
+    rollRocksNorth (x : xs) = x : rollRocksNorth xs
+    rollRocksNorth [] = []
 
-        fix f a = if applied == a then a else fix f applied
-            where
-                applied = f a
+    fix f a = if applied == a then a else fix f applied
+      where
+        applied = f a
 
-load = sum . zipWith beamLoad [1..] . reverse
-    where
-        beamLoad lever beam = lever * length (filter (==Round) beam)
+tiltSouth = reverse . tiltNorth . reverse
+
+tiltWest = transpose . tiltNorth . transpose
+
+tiltEast = transpose . tiltSouth . transpose
+
+tiltCycle =  tiltEast . tiltSouth . tiltWest . tiltNorth
+
+tiltCycles :: Int -> Dish -> Dish
+tiltCycles = tiltCycles' mempty
+    where 
+        tiltCycles' :: Map.Map Dish Int -> Int -> Dish -> Dish
+        tiltCycles' _ 0 d = d
+        tiltCycles' seen n d 
+            | d `Map.member` seen = tiltCycles (n `mod` (seen Map.! d - n)) d
+            | otherwise = tiltCycles' (Map.insert d n seen) (n-1) (tiltCycle d)
+
+load = sum . zipWith beamLoad [1 ..] . reverse
+  where
+    beamLoad lever beam = lever * length (filter (== Round) beam)
 
 dishParser :: Parser Dish
 dishParser = rowParser `sepEndBy` space
