f6dcf4b3658275f3b67642aacb4a6c22c4150be5
Mr-Pine <git@mr-pine.de>
2023-12-21 14:56:25 +0100

    Day 21 Part 1 but fast

--- app/Day21/StepCounter.hs
+++ app/Day21/StepCounter.hs
@@ -1,26 +1,29 @@
 {-# LANGUAGE TupleSections #-}
 module Day21.StepCounter (solveDay21) where
 import Data.Array (listArray, Array, Ix (inRange), bounds, (!))
-import Data.List (transpose, find, nub)
+import Data.List (transpose, find, nub, singleton)
 import Data.Maybe (fromJust)
 import Util (example, input)
+import qualified Data.Set as Set
+import Data.Bifunctor (Bifunctor(bimap))
 
 solveDay21 = do
     putStrLn "Day 21 - Step Counter:"
     input <- input 21
     let (tiles, start) = parseMap input
-    print . day1 tiles $ [start]
+    print . day1 tiles $ start
 
-day1 tiles = length . steps tiles 64 
+day1 tiles = length . steps tiles 64 .  Set.singleton 
 
 data Tile = Garden | Rock deriving (Show, Eq)
 
 steps tileMap 0 ps = ps
 steps tileMap r ps = steps tileMap (r - 1) (step tileMap ps)
 
-step tileMap = nub . concatMap singleStep
+step tileMap = foldMap singleStep
     where
-        singleStep (x,y) = filter ((==Garden) . (tileMap !)) $ filter (inRange . bounds $ tileMap) [(x,y-1), (x,y+1), (x-1,y), (x+1,y)]
+        singleStep (x,y) = Set.fromList . filter ((==Garden) . (tileMap !)) . map (`remainder` (snd $ bounds tileMap)) $ [(x,y-1), (x,y+1), (x-1,y), (x+1,y)]
+        (x,y) `remainder` (bx,by) = (x `rem` bx, y `rem` by)
 
 parseMap :: String -> (Array (Int, Int) Tile, (Int, Int))
 parseMap tileMap = (array, fromJust findStart)
