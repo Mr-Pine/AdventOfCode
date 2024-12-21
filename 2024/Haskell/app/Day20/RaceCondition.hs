1feffab6c498522fc7a0e9873b8d77ec51d9b4ae
Mr-Pine <git@mr-pine.de>
2024-12-21 15:28:34 +0100

    Day 20 but better (and Part 2)

--- 2024/Haskell/app/Day20/RaceCondition.hs
+++ 2024/Haskell/app/Day20/RaceCondition.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TupleSections #-}
 
 module Day20.RaceCondition (solveDay20) where
@@ -6,72 +5,43 @@ module Day20.RaceCondition (solveDay20) where
 import Data.Array (inRange)
 import Data.Hashable (Hashable)
 import Data.List (find)
-import Data.Map (assocs)
+import Data.Map ((!), (!?), fromList)
 import qualified Data.Map as Map
 import Data.Maybe (fromJust)
-import Data.Set (Set, fromList, member)
-import Data.Tuple.HT (mapFst, mapSnd)
-import GHC.Generics (Generic)
+import Data.Set (Set, member, toList, union, empty, size)
 import Util
+import qualified Data.Set as Set
+import Data.Tuple (swap)
 
 solveDay20 input _ = do
     putStrLn "Day 20 - Race Condition:"
-    --let pathBlocks = maze input
-    --let endPos = end input
-    --let startPos = start input
-    --let avoid = [((6, 7), (5, 7))]
-    --let (shortestPathLen, pred) = dijkstra (cheatyNeighbours avoid pathBlocks (bounds input)) (startPos, Unused) (endPos, Used)
-    --print shortestPathLen
-    --print . extractCheats (findAllOnShortestPath (endPos, Used) pred) $ pred
     print . part1 $ input
+    print . part2 $ input
 
-maze = fromList . map fst . concatMap (filter ((/= '#') . snd)) . xyEnumerate . lines
+maze = Set.fromList . map fst . concatMap (filter ((/= '#') . snd)) . xyEnumerate . lines
 bounds = (\ls -> ((0, 0), (length (head ls) - 1, length ls - 1))) . lines
 start = fst . fromJust . find ((== 'S') . snd) . concat . xyEnumerate . lines
 end = fst . fromJust . find ((== 'E') . snd) . concat . xyEnumerate . lines
 
-part1 input = length . filter ((>=100) . fst) . map (mapFst (baseline -)) $ findAllCheats baseline dijkstraAvoiding shortestPathFinder []
+part1 input = size . Set.filter ((>=100) . fst) $ findCheats input 2
+part2 input = size . Set.filter ((>=100) . fst) $ findCheats input 20
+
+findCheats input cheatLength = allCheats
     where
-        free = maze input
-        startPos = start input
-        endPos = end input
+        defaultPath = dijkstra (defaultNeigbours (maze input)) (start input) (end input)
+        path = zip [0..] . reverse . (`buildPath` end input) . snd $ defaultPath
+        pathSet = Set.fromList . map snd $ path
+        pathMap = fromList . map swap $ path
         mazeBounds = bounds input
 
-        baseline = fst $ dijkstra (defaultNeigbours free) startPos endPos
+        allCheats = Set.filter ((>0) . fst) . Set.foldr union empty . Set.map cheats $ pathSet
 
-        dijkstraAvoiding avoid = dijkstra (cheatyNeighbours avoid free mazeBounds) (startPos, Unused) (endPos, Used)
-        shortestPathFinder = findAllOnShortestPath (endPos, Used)
+        evalCheat from to = (pathMap ! to - pathMap ! from - manhattanDistance from to, (from,to))
 
-defaultNeigbours :: Set Position -> Position -> [(Int, Position)]
+        cheats from = Set.map (evalCheat from) .  Set.filter ((<= cheatLength) . manhattanDistance from) $ pathSet
+
+        manhattanDistance (x1,y1) (x2,y2) = abs (x1 - x2) + abs (y1 - y2)
+
+        buildPath preds pos = pos : maybe [] (buildPath preds . head) (preds !? pos)
+
 defaultNeigbours available = map (1,) . filter (`member` available) . flip map directions . flip moveInDirection
-
-data CheatStatus = Unused | Active | Used deriving (Enum, Generic, Eq, Ord, Show)
-instance Hashable CheatStatus
-
-type CheatablePos = (Position, CheatStatus)
-
-cheatyNeighbours _ noWalls bounds (pos, Unused) = (0, (pos, Active)) : normalNeighbours pos ++ cheatingNeighbours pos
-  where
-    normalNeighbours = map (mapSnd (,Unused)) . defaultNeigbours noWalls
-    cheatingNeighbours = map (mapSnd (,Active) . (1,)) . filter (inRange bounds) . flip map directions . flip moveInDirection
-cheatyNeighbours avoid noWalls bounds (pos, Active)
-    | pos `member` noWalls = (0, (pos, Used)) : (map (mapSnd (,Used)) . filter (not . (`elem` avoidingExits) . snd) . defaultNeigbours noWalls $ pos)
-    | otherwise = map (mapSnd (,Used)) . filter (not . (`elem` avoidingExits) . snd) . defaultNeigbours noWalls $ pos
-  where
-    avoidingExits = map snd . filter ((== pos) . fst) $ avoid
-cheatyNeighbours _ noWalls bounds (pos, _) = map (mapSnd (,Used)) . defaultNeigbours noWalls $ pos
-
-extractCheats onShortestPath = map unpack . concatMap (\(a, bs) -> map (,a) bs) . assocs . exits
-  where
-    unpack = mapFst fst . mapSnd fst
-    exits = Map.filter (not . null) . Map.map (filter (`member` onShortestPath) . filter ((== Active) . snd)) . Map.filterWithKey (const . (`member` onShortestPath))
-
-findAllCheats baseline dijkstraAvoiding shortestPathFinder cheats
-    | pathLen == baseline = cheats
-    | otherwise = findAllCheats baseline dijkstraAvoiding shortestPathFinder (newCheats ++ cheats)
-  where
-    (pathLen, predecessors) = dijkstraAvoiding (map snd cheats)
-    newCheats = map (pathLen,) $ extractCheats (shortestPathFinder predecessors) predecessors
-
-type Position = (Int, Int)
-type Cheat = (Position, Position)
