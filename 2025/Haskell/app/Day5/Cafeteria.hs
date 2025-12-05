839489eb697483029f96f5c665b435cef2d3967b
MisterPine <git@mr-pine.de>
2025-12-05 09:00:51 +0100

    Day 5 Part 2

--- 2025/Haskell/app/Day5/Cafeteria.hs
+++ 2025/Haskell/app/Day5/Cafeteria.hs
@@ -4,24 +4,28 @@ import qualified Text.Megaparsec.Char.Lexer as Lex
 import Util
 import Text.Megaparsec.Char (newline, char)
 import Text.Megaparsec (sepEndBy1)
+import Data.List.Utils (uniq)
+import Data.Range (Range (..), (+=+), inRanges, fromRanges, mergeRanges, Bound (..), BoundType (..))
 
 solveDay5 input _ = do
     putStrLn "Day 5 - Cafeteria:"
     inventory <- parseOrError inventoryParser input
     print . uncurry part1 $ inventory
+    print . uncurry (const . part2) $ inventory
 
-part1 ranges = length . filter (flip any ranges . flip contains)
+part1 ranges = length . filter (inRanges ranges)
+part2 = sum . map entryCount . mergeRanges
 
 type IngredientId = Int
-
-data IdRange = IdRange IngredientId IngredientId deriving Show
-contains (IdRange lower upper) i = i >= lower && i <= upper
+type IdRange = Range IngredientId
+entryCount (SingletonRange _) = 1
+entryCount (SpanRange (Bound x Inclusive) (Bound y Inclusive)) = y - x + 1
 
 idParser :: Parser IngredientId
 idParser = Lex.decimal
 
 idRangesParser :: Parser [IdRange]
-idRangesParser = (IdRange <$> idParser <* char '-' <*> idParser) `sepEndBy1` newline
+idRangesParser = ((+=+) <$> idParser <* char '-' <*> idParser) `sepEndBy1` newline
 
 inventoryParser :: Parser ([IdRange], [IngredientId])
 inventoryParser = (,) <$> idRangesParser <* newline <*> (idParser `sepEndBy1` newline)
